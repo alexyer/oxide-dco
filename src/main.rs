63ff14a218611d7cc3d046e526a68ce4b86973de
Olexander Yermakov <olexander.yermakov@gmail.com>
2020-05-10 03:44:55 +0300

    Split generation and measurement

--- src/main.rs
+++ src/main.rs
@@ -10,14 +10,15 @@ use stm32f1xx_hal as hal;
 use crate::hal::{
     adc, gpio,
     gpio::{gpiob, Output, PushPull},
-    pac::{interrupt, Interrupt, Peripherals, ADC1, GPIOA, TIM3},
+    pac::{interrupt, Interrupt, Peripherals, ADC1, GPIOA, TIM2, TIM3},
     prelude::*,
     rcc::Enable,
     timer::{CountDownTimer, Event, Timer},
 };
 
-use cortex_m::{asm::wfi, interrupt::Mutex};
+use core::sync::atomic::{AtomicU32, Ordering};
 
+use cortex_m::{asm::wfi, interrupt::Mutex};
 use cortex_m::peripheral::Peripherals as c_m_Peripherals;
 use cortex_m_rt::entry;
 
@@ -26,12 +27,17 @@ use eurorack_oxide_utils::voct::Voltage;
 
 type AdcCh = gpiob::PB0<gpio::Analog>;
 type OUTPIN = gpiob::PB1<Output<PushPull>>;
+const AVGBUFSIZE: usize = 1024;
+const TIM3_FREQ_HZ: u32 = 200000;
+const SEC_IN_US: u32 = 1000000;
 
 static G_ADC1: Mutex<RefCell<Option<adc::Adc<ADC1>>>> = Mutex::new(RefCell::new(None));
 static G_CH0: Mutex<RefCell<Option<AdcCh>>> = Mutex::new(RefCell::new(None));
 static G_DAC: Mutex<RefCell<Option<GPIOA>>> = Mutex::new(RefCell::new(None));
-static G_TIM: Mutex<RefCell<Option<CountDownTimer<TIM3>>>> = Mutex::new(RefCell::new(None));
+static G_TIM2: Mutex<RefCell<Option<CountDownTimer<TIM2>>>> = Mutex::new(RefCell::new(None));
+static G_TIM3: Mutex<RefCell<Option<CountDownTimer<TIM3>>>> = Mutex::new(RefCell::new(None));
 static G_OUT: Mutex<RefCell<Option<OUTPIN>>> = Mutex::new(RefCell::new(None));
+static PERIOD: AtomicU32 = AtomicU32::new(0);
 
 macro_rules! get_peripheral_ref {
     ($lref:ident, $gref:ident) => {
@@ -41,51 +47,86 @@ macro_rules! get_peripheral_ref {
     };
 }
 
+const fn cirle_time() -> u32 {
+    SEC_IN_US / TIM3_FREQ_HZ
+}
+
+fn us_to_period(us: u32) -> u32 {
+    us / cirle_time() / 2
+}
+
 #[interrupt]
-fn TIM3() {
+fn TIM2() {
     static mut ADC: Option<adc::Adc<ADC1>> = None;
     static mut CH0: Option<AdcCh> = None;
-    static mut DAC: Option<GPIOA> = None;
-    static mut TIM: Option<CountDownTimer<TIM3>> = None;
-    static mut OUT: Option<OUTPIN> = None;
-
-    static mut COUNTER: usize = 0;
-    static mut PERIOD: usize = 0;
+    static mut TIM: Option<CountDownTimer<TIM2>> = None;
+    static mut AVG_COUNTER: usize = 0;
+    static mut AVGBUF: [u16; AVGBUFSIZE] = [0; AVGBUFSIZE];
 
     let adc1 = get_peripheral_ref!(ADC, G_ADC1);
     let ch0 = get_peripheral_ref!(CH0, G_CH0);
-    let dac = get_peripheral_ref!(DAC, G_DAC);
-    let out = get_peripheral_ref!(OUT, G_OUT);
-    let tim = get_peripheral_ref!(TIM, G_TIM);
+    let tim = get_peripheral_ref!(TIM, G_TIM2);
 
-    *COUNTER += 1;
-    if *COUNTER == 1 {
-        out.toggle().ok();
-        let oct = 0000.0;
-        // let measure: u16 = adc1.read(ch0).unwrap();
-        let avg = avg(adc1, ch0);
-        let mv = MvOct(avg as f32 * 1.3333333 + oct + 1000.0);
-        *PERIOD = mv.us() as usize;
-        // dac.odr.write(|w| unsafe { w.bits((measure / 32) as u32) });
-        dac.odr
-            .write(|w| unsafe { w.bits((mv.hz() / 32.0) as u32) });
-    } else if *COUNTER == *PERIOD / 2 {
-        out.toggle().ok();
-    } else if *COUNTER == *PERIOD {
-        *COUNTER = 0;
+    AVGBUF[*AVG_COUNTER % AVGBUFSIZE] = adc1.read(ch0).unwrap();
+    *AVG_COUNTER += 1;
+
+    if *AVG_COUNTER % AVGBUFSIZE == 0 {
+        let avg = avg(AVGBUF);
+        let voltage = avg * 1200 / adc1.read_vref() as u32;
+        let mv = MvOct(voltage as f32 * 1.5015 as f32);
+        PERIOD.store(us_to_period(mv.us()), Ordering::Relaxed);
     }
 
     tim.wait().ok();
 }
 
-fn avg(adc: &mut adc::Adc<ADC1>, ch: &mut AdcCh) -> u32 {
+#[interrupt]
+fn TIM3() {
+    // static mut DAC: Option<GPIOA> = None;
+    static mut TIM: Option<CountDownTimer<TIM3>> = None;
+    static mut OUT: Option<OUTPIN> = None;
+
+    static mut LOCAL_PERIOD: u32 = 0;
+    static mut COUNTER: u32 = 0;
+
+    // let dac = get_peripheral_ref!(DAC, G_DAC);
+    let out = get_peripheral_ref!(OUT, G_OUT);
+    let tim = get_peripheral_ref!(TIM, G_TIM3);
+
+    if *COUNTER >= *LOCAL_PERIOD {
+        out.toggle().ok();
+        *COUNTER = 0;
+        *LOCAL_PERIOD = PERIOD.load(Ordering::Relaxed);
+    }
+    *COUNTER += 1;
+
+    // if *COUNTER == 1 {
+    //     out.toggle().ok();
+    //     // let oct = 0000.0;
+    //     // let avg = avg(AVGBUF);
+    //     // let avg: u16 = adc1.read(ch0).unwrap();
+    //     // let voltage = avg * 1200 / adc1.read_vref();
+    //     let mv = MvOct(7000.0);
+    //     // let mv = MvOct(voltage as f32 * 1.5015 as f32 + oct + 2000.0);
+    //     *PERIOD = (mv.us() / 10 * 2) as usize;
+    //     // dac.odr.write(|w| unsafe { w.bits((measure / 32) as u32) });
+    //     // dac.odr
+    //     //     .write(|w| unsafe { w.bits((mv.hz() / 32.0) as u32) });
+    // } else if *COUNTER == *PERIOD / 2 {
+    //     out.toggle().ok();
+    // } else if *COUNTER == *PERIOD {
+    //     *COUNTER = 0;
+    // }
+    tim.wait().ok();
+}
+
+fn avg(buf: &mut [u16; AVGBUFSIZE]) -> u32 {
     let mut acc: u32 = 0;
-    for i in 0..1024 {
-        let measure: u32 = adc.read(ch).unwrap();
-        acc += measure;
+    for i in 0..AVGBUFSIZE {
+        acc += buf[i] as u32;
     }
 
-    acc / 1024
+    acc / AVGBUFSIZE as u32
 }
 
 #[entry]
@@ -107,8 +148,8 @@ fn init_peripherals() {
             let clocks = rcc
                 .cfgr
                 .adcclk(10.mhz())
-                .sysclk(70.mhz())
-                .pclk1(30.mhz())
+                .sysclk(30.mhz())
+                .pclk1(15.mhz())
                 .freeze(&mut flash.acr);
 
             // Init ADC
@@ -121,16 +162,26 @@ fn init_peripherals() {
             *G_CH0.borrow(cs).borrow_mut() = Some(ch0);
 
             // Init timer
-            let mut tim = Timer::tim3(dp.TIM3, &clocks, &mut rcc.apb1).start_count_down(8.mhz());
-            tim.listen(Event::Update);
+            let mut tim2 = Timer::tim2(dp.TIM2, &clocks, &mut rcc.apb1)
+                .start_count_down((TIM3_FREQ_HZ / 2).hz());
+            tim2.listen(Event::Update);
 
-            *G_TIM.borrow(cs).borrow_mut() = Some(tim);
+            let mut tim3 =
+                Timer::tim3(dp.TIM3, &clocks, &mut rcc.apb1).start_count_down(TIM3_FREQ_HZ.hz());
+            tim3.listen(Event::Update);
+
+            *G_TIM2.borrow(cs).borrow_mut() = Some(tim2);
+            *G_TIM3.borrow(cs).borrow_mut() = Some(tim3);
 
             let mut nvic = cp.NVIC;
             unsafe {
                 nvic.set_priority(Interrupt::TIM3, 1);
                 cortex_m::peripheral::NVIC::unmask(Interrupt::TIM3);
+
+                nvic.set_priority(Interrupt::TIM2, 16);
+                cortex_m::peripheral::NVIC::unmask(Interrupt::TIM2);
             }
+            cortex_m::peripheral::NVIC::unpend(Interrupt::TIM2);
             cortex_m::peripheral::NVIC::unpend(Interrupt::TIM3);
 
             // Configure out pin
