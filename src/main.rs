a45fd6349432b118f884dd659f593e6c6253431e
Olexander Yermakov <mannavard1611@gmail.com>
2020-05-19 01:07:44 +0300

    Add encoder (#4)
    Co-authored-by: Olexander Yermakov <olexander.yermakov@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -18,7 +18,7 @@ use crate::hal::{
     timer::{CountDownTimer, Event, Timer},
 };
 
-use core::sync::atomic::{AtomicU32, Ordering};
+use core::sync::atomic::{AtomicI16, AtomicU32, Ordering};
 
 use eurorack_oxide_utils::voct::MvOct;
 use eurorack_oxide_utils::voct::Voltage;
@@ -26,6 +26,7 @@ use eurorack_oxide_utils::voct::Voltage;
 const AVG_BUF_SIZE: usize = 32;
 const TIM3_FREQ_HZ: u32 = 200000;
 const SEC_IN_US: u32 = 1000000;
+const FINE_TUNE_STEP: i16 = 2;
 
 const fn circle_time() -> u32 {
     SEC_IN_US / TIM3_FREQ_HZ
@@ -49,7 +50,8 @@ const APP: () = {
     struct Resources {
         adc1: adc::Adc<pac::ADC1>,
         ch0: gpio::gpiob::PB0<gpio::Analog>,
-        dac: pac::GPIOA,
+        exti: pac::EXTI,
+        gpioa: pac::GPIOA,
         hard_sync: gpio::gpiob::PB5<gpio::Input<gpio::Floating>>,
         out: gpio::gpiob::PB1<gpio::Output<gpio::PushPull>>,
         tim2: CountDownTimer<pac::TIM2>,
@@ -60,6 +62,9 @@ const APP: () = {
 
         #[init(AtomicU32::new(0))]
         counter: AtomicU32,
+
+        #[init(AtomicI16::new(0))]
+        fine_tune: AtomicI16,
 
         #[init(AtomicU32::new(0))]
         period: AtomicU32,
@@ -98,9 +103,9 @@ const APP: () = {
         let out = gpiob.pb1.into_push_pull_output(&mut gpiob.crl);
 
         // Init DAC port
-        let dac = cx.device.GPIOA;
+        let gpioa = cx.device.GPIOA;
         pac::GPIOA::enable(&mut rcc.apb2);
-        dac.crl.write(|w| unsafe { w.bits(0x33333333) });
+        gpioa.crl.write(|w| unsafe { w.bits(0x33333333) });
 
         // Init Hard Sync pin
         let mut hard_sync = gpiob.pb5.into_floating_input(&mut gpiob.crl);
@@ -108,10 +113,39 @@ const APP: () = {
         hard_sync.trigger_on_edge(&cx.device.EXTI, gpio::Edge::RISING);
         hard_sync.enable_interrupt(&cx.device.EXTI);
 
+        // Init Encoder
+        // Into pull up input
+        gpioa.crh.write(|w| unsafe { w.bits(0x8800) });
+        gpioa.bsrr.write(|w| unsafe { w.bits(1 << 10) });
+        gpioa.bsrr.write(|w| unsafe { w.bits(1 << 11) });
+
+        // Make interrupt source
+        afio.exticr3
+            .exticr3()
+            .modify(|r, w| unsafe { w.bits((r.bits() & !(0xf << 10)) | (0 << 10)) });
+
+        // Trigger on Falling edge
+        cx.device
+            .EXTI
+            .ftsr
+            .modify(|r, w| unsafe { w.bits(r.bits() | (1 << 10)) });
+        cx.device
+            .EXTI
+            .rtsr
+            .modify(|r, w| unsafe { w.bits(r.bits() & !(1 << 10)) });
+
+        // Enable EXTI interrupt
+        cx.device
+            .EXTI
+            .imr
+            .modify(|r, w| unsafe { w.bits(r.bits() | (1 << 10)) });
+        let exti = cx.device.EXTI;
+
         init::LateResources {
             adc1,
             ch0,
-            dac,
+            exti,
+            gpioa,
             hard_sync,
             out,
             tim2,
@@ -119,13 +153,33 @@ const APP: () = {
         }
     }
 
-    #[task(binds = EXTI9_5, priority = 2, resources = [&counter, hard_sync])]
+    #[task(binds = EXTI15_10, priority = 1, resources = [exti, &fine_tune, gpioa])]
+    fn encoder_handler(mut cx: encoder_handler::Context) {
+        let bits = cx.resources.gpioa.lock(|gpioa| gpioa.idr.read().bits());
+
+        let state = (bits & (1 << 11)) == 0;
+
+        match state {
+            true => cx
+                .resources
+                .fine_tune
+                .fetch_add(FINE_TUNE_STEP, Ordering::Relaxed),
+            false => cx
+                .resources
+                .fine_tune
+                .fetch_add(-FINE_TUNE_STEP, Ordering::Relaxed),
+        };
+
+        cx.resources.exti.pr.write(|w| unsafe { w.bits(1 << 10) });
+    }
+
+    #[task(binds = EXTI9_5, priority = 3, resources = [&counter, hard_sync])]
     fn hard_sync(cx: hard_sync::Context) {
         cx.resources.counter.store(0, Ordering::Relaxed);
         cx.resources.hard_sync.clear_interrupt_pending_bit();
     }
 
-    #[task(binds = TIM3, priority = 3, resources = [&counter, out, tim3, &period])]
+    #[task(binds = TIM3, priority = 4, resources = [&counter, out, tim3, &period])]
     fn tick(cx: tick::Context) {
         let c = cx.resources.counter.load(Ordering::Relaxed);
 
@@ -141,7 +195,7 @@ const APP: () = {
         cx.resources.tim3.clear_update_interrupt_flag();
     }
 
-    #[task(binds = TIM2, priority = 1, resources = [adc1, avg_buf, ch0, dac, &period, tim2])]
+    #[task(binds = TIM2, priority = 2, resources = [adc1, avg_buf, ch0, gpioa, &fine_tune, &period, tim2])]
     fn measure(cx: measure::Context) {
         static mut AVG_COUNTER: usize = 0;
 
@@ -153,17 +207,17 @@ const APP: () = {
             let avg = avg(cx.resources.avg_buf);
             let voltage = avg as f32 * 1191.55555 / cx.resources.adc1.read_vref() as f32;
             // let voltage = avg as f32 * 1.237740204;
-            let mv = MvOct(6000.0 - 2.0 * voltage);
+            let mv = MvOct(6000.0 - 2.0 * voltage)
+                + cx.resources.fine_tune.load(Ordering::Relaxed) as f32;
             // let mv = MvOct(voltage as f32 * 1.5015 as f32);
 
             cx.resources
                 .period
                 .store(us_to_period(mv.us()), Ordering::Relaxed);
 
-            cx.resources
-                .dac
-                .odr
-                .write(|w| unsafe { w.bits((mv.hz() / 16.0) as u32) });
+            cx.resources.gpioa.odr.modify(|r, w| unsafe {
+                w.bits((r.bits() & (0xff << 8)) | (mv.hz() / 16.0) as u32 & 0xff)
+            });
         }
 
         cx.resources.tim2.clear_update_interrupt_flag();
