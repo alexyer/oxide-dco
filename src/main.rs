c8c94a349b130f30b8adfc7b9102466b795e3d81
Olexander Yermakov <olexander.yermakov@gmail.com>
2020-09-05 10:52:51 +0300

    Add display

--- src/main.rs
+++ src/main.rs
@@ -6,12 +6,24 @@ use panic_semihosting as _;
 
 use rtfm::app;
 
+use cortex_m::peripheral::DWT;
+use embedded_graphics::{
+    egcircle,
+    fonts::{Font6x8, Text},
+    pixelcolor::BinaryColor,
+    prelude::*,
+    primitives::Circle,
+    primitive_style,
+    style::{TextStyleBuilder, PrimitiveStyle, Styled},
+};
 use embedded_hal::digital::v2::OutputPin;
+use ssd1306::{prelude::*, Builder};
 use stm32f1xx_hal as hal;
 
 use crate::hal::{
     adc, gpio,
     gpio::ExtiPin,
+    i2c::{BlockingI2c, DutyCycle, Mode},
     pac,
     prelude::*,
     rcc::Enable,
@@ -20,17 +32,30 @@ use crate::hal::{
 
 use core::sync::atomic::{AtomicI16, AtomicU32, Ordering};
 
+use embedded_graphics::style::TextStyle;
 use eurorack_oxide_utils::voct::MvOct;
 use eurorack_oxide_utils::voct::Voltage;
+use rtfm::cyccnt::{U32Ext, Instant};
+use stm32f1xx_hal::delay::Delay;
+use core::fmt::Binary;
 
 const AVG_BUF_SIZE: usize = 32;
 const TIM3_FREQ_HZ: u32 = 200000;
 const SEC_IN_US: u32 = 1000000;
 const FINE_TUNE_STEP: i16 = 2;
+const SYSCLK_FREQ_HZ: u32 = 30_000_000;
 
 const fn circle_time() -> u32 {
     SEC_IN_US / TIM3_FREQ_HZ
 }
+
+type I2C = BlockingI2c<
+    pac::I2C1,
+    (
+        gpio::gpiob::PB8<gpio::Alternate<gpio::OpenDrain>>,
+        gpio::gpiob::PB9<gpio::Alternate<gpio::OpenDrain>>,
+    ),
+>;
 
 fn us_to_period(us: u32) -> u32 {
     us / circle_time() / 2
@@ -45,11 +70,61 @@ fn avg(buf: &mut [u16; AVG_BUF_SIZE]) -> u32 {
     acc / AVG_BUF_SIZE as u32
 }
 
+fn gui_intro(display: &mut GraphicsMode<I2cInterface<I2C>>) {
+    let text_style = TextStyleBuilder::new(Font6x8)
+            .text_color(BinaryColor::On)
+            .build();
+
+    let size = display.size();
+
+    Text::new("RUDEBOY", Point::new((size.width / 2 - 21) as i32, (size.height / 2) as i32))
+        .into_styled(text_style)
+        .draw(display)
+        .unwrap();
+
+    display.flush().unwrap();
+}
+
+fn gui_show(display: &mut GraphicsMode<I2cInterface<I2C>>) {
+    display.clear();
+
+    let text_style = TextStyleBuilder::new(Font6x8)
+        .text_color(BinaryColor::On)
+        .build();
+
+    let size = display.size();
+
+    let circle_1: Styled<Circle, PrimitiveStyle<BinaryColor>> = egcircle!(
+    center = Point::new((size.width / 2) as i32, size.height as i32),
+    radius = size.width / 3,
+    style = primitive_style!(
+        stroke_color = BinaryColor::On,
+        stroke_width = 2
+       )
+    );
+
+    circle_1.draw(display).unwrap();
+
+    Text::new("-12", Point::new(1_i32, (size.height - 8) as i32))
+        .into_styled(text_style)
+        .draw(display)
+        .unwrap();
+
+    Text::new("+12", Point::new((size.width - 18) as i32, (size.height - 8) as i32))
+        .into_styled(text_style)
+        .draw(display)
+        .unwrap();
+
+    display.flush().unwrap();
+}
+
 #[app(device = stm32f1xx_hal::pac, peripherals = true)]
 const APP: () = {
     struct Resources {
         adc1: adc::Adc<pac::ADC1>,
         ch0: gpio::gpiob::PB0<gpio::Analog>,
+        delay: Delay,
+        display: GraphicsMode<I2cInterface<I2C>>,
         exti: pac::EXTI,
         gpioa: pac::GPIOA,
         hard_sync: gpio::gpiob::PB5<gpio::Input<gpio::Floating>>,
@@ -80,7 +155,7 @@ const APP: () = {
         let clocks = rcc
             .cfgr
             .adcclk(10.mhz())
-            .sysclk(30.mhz())
+            .sysclk(SYSCLK_FREQ_HZ.hz())
             .pclk1(15.mhz())
             .freeze(&mut flash.acr);
 
@@ -141,9 +216,37 @@ const APP: () = {
             .modify(|r, w| unsafe { w.bits(r.bits() | (1 << 10)) });
         let exti = cx.device.EXTI;
 
+        // Init display
+        let scl = gpiob.pb8.into_alternate_open_drain(&mut gpiob.crh);
+        let sda = gpiob.pb9.into_alternate_open_drain(&mut gpiob.crh);
+
+        let mut i2c = BlockingI2c::i2c1(
+            cx.device.I2C1,
+            (scl, sda),
+            &mut afio.mapr,
+            Mode::Fast {
+                frequency: 400_000.hz(),
+                duty_cycle: DutyCycle::Ratio2to1,
+            },
+            clocks,
+            &mut rcc.apb1,
+            1000,
+            10,
+            1000,
+            1000,
+        );
+
+        let cp = cortex_m::Peripherals::take().unwrap();
+        let delay = Delay::new(cp.SYST, clocks);
+
+        let mut display: GraphicsMode<_> = Builder::new().connect_i2c(i2c).into();
+        display.init().unwrap();
+
         init::LateResources {
             adc1,
             ch0,
+            delay,
+            display,
             exti,
             gpioa,
             hard_sync,
@@ -152,6 +255,17 @@ const APP: () = {
             tim3,
         }
     }
+
+    #[idle(resources = [delay, display])]
+    fn idle(cx: idle::Context) -> ! {
+        gui_intro(cx.resources.display);
+        cx.resources.delay.delay_ms(500_u16);
+        gui_show(cx.resources.display);
+
+        loop {
+            cortex_m::asm::wfi();
+        }
+}
 
     #[task(binds = EXTI15_10, priority = 1, resources = [exti, &fine_tune, gpioa])]
     fn encoder_handler(mut cx: encoder_handler::Context) {
